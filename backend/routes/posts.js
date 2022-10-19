7e290e4393ee6eec94d8bcf5e82ff2ff69444e2a
nafiu-dev <nafiu.developer@gmail.com>
2022-10-19 22:49:17 +0500

    backend v1

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -41,29 +41,6 @@ router.get('/followers-posts', verifyAuth, async (req, res) => {
 })
 
 
-
-// POST | /api/v1/add-new| private | add a new post
-router.post('/add-new', verifyAuth, async (req, res) => {
-    try {
-
-        const newPost = await Post.create({
-            UserId: req.user.id,
-            title: req.body.title,
-            description: req.body.description,
-            post_image: req.body.image_url,
-        })
-
-        res.status(200).json({
-            data: newPost,
-            success: true
-        })
-    } catch (err) {
-        console.log(err)
-        res.status(400).json({success: false})
-    }
-})
-
-
 // GET | /api/v1/post/:id | public | get a single post by id
 router.get('/post/:id', async (req, res) => {
     try {
@@ -86,34 +63,27 @@ router.get('/post/:id', async (req, res) => {
 })
 
 
-// DELETE | /api/v1/post/delete-post/:id | Private | delete a post
-router.delete('/delete-post/:id',verifyAuth, async (req, res) =>{
+
+// POST | /api/v1/add-new| private | add a new post
+router.post('/add-new', verifyAuth, async (req, res) => {
     try {
-        const post = await Post.findById(req.params.id)
-        if (!post) {
-            return res.status(400).json({
-                success: false
-            })
-        }
-        
-        if(!post.UserId == req.user.id){
-            return res.status(400).json({
-                success: false
-            })
-        }else{
-            await post.delete()
-        }
-        
-        res.status(200).json({ 
-            success: true,
-            data: {}
+
+        const newPost = await Post.create({
+            UserId: req.user.id,
+            title: req.body.title,
+            description: req.body.description,
+            post_image: req.body.image_url,
+        })
+
+        res.status(200).json({
+            data: newPost,
+            success: true
         })
     } catch (err) {
         console.log(err)
         res.status(400).json({success: false})
     }
 })
-
 
 // PUT | /api/v1/post/edit-post/:id| Private | Edit a post
 router.put('/edit-post/:id', verifyAuth, async (req, res) =>{
@@ -147,6 +117,37 @@ router.put('/edit-post/:id', verifyAuth, async (req, res) =>{
         res.status(400).json({success: false})
     }
 })
+
+// DELETE | /api/v1/post/delete-post/:id | Private | delete a post
+router.delete('/delete-post/:id',verifyAuth, async (req, res) =>{
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            return res.status(400).json({
+                success: false
+            })
+        }
+        
+        if(!post.UserId == req.user.id){
+            return res.status(400).json({
+                success: false
+            })
+        }else{
+            await post.delete()
+        }
+        
+        res.status(200).json({ 
+            success: true,
+            data: {}
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({success: false})
+    }
+})
+
+
+
 
 module.exports = router 
 
